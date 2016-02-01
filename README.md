b0e25915a2b9066ab4bb65a23d3ea063e2d2e443
AnweshReddyThati <chunki.reddy@gmail.com>
2016-01-31 17:09:58 -0800

    first comment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # webLab
+
+
+
+hi How are you
---
+++ appml.php
@@ -0,0 +1,12 @@
+{"user" : 0, "recPos" : 1, "fromRec" : 1, "toRec" : 7, "totalRecCounter" : 32, "records" : [{"Title" : "Empire Burlesque","Artist" : "Bob Dylan","Price" : "10.90"},{"Title" : "Hide your heart","Artist" : "Bonnie Tyler","Price" : "9.90"},{"Title" : "Greatest Hits","Artist" : "Dolly Parton","Price" : "9.90"},{"Title" : "Still got the blues","Artist" : "Gary Moore","Price" : "10.20"},{"Title" : "Eros","Artist" : "Eros Ramazzotti","Price" : "9.90"},{"Title" : "One night only","Artist" : "Bee Gees","Price" : "10.90"},{"Title" : "Sylvias Mother","Artist" : "Dr.Hook","Price" : "8.10"}], "dateFormat" : "yyyy-mm-dd", 
+"rowsperpage" : 7,
+"data" : {
+    "type" : "csvfile",
+    "filename" : "cd_catalog.txt",
+    "items" : [
+        {"name" : "Title", "index" : 1},
+        {"name" : "Artist", "index" : 2},
+        {"name" : "Price", "index" : 5}
+    ]
+}
+}
